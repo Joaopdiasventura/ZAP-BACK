6d477c701f14ad597b6da774448f530d32962f58
João Paulo Dias Ventura <joaopdiasventura@gmail.com>
2023-12-03 13:00:48 -0300

    tentando

--- app.js
+++ app.js
@@ -11,32 +11,41 @@ import bodyParser from "body-parser";
 const app = express();
 const port = process.env.PORT || 3000;
 
-
+// Configurar a sessão
 app.use(session({
     secret: 'Jpplay2_0',
     resave: true,
     saveUninitialized: true
 }));
 
+// Habilitar o CORS para todas as origens
 app.use(cors());
 
+// Utilizar bodyParser para analisar corpos de solicitação JSON
 app.use(bodyParser.json());
 
+// Adicionar rotas
 app.use(send);
 app.use(view);
 
+// Configurar autenticação com Passport
 logar(passport);
 
+// Conectar ao MongoDB
 const mongoDBURI = "mongodb+srv://joaopdiasventura:Jpplay2_0@cluster0.7i4iw97.mongodb.net/zapzap";
 
 mongoose.connect(mongoDBURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
+})
+.then(() => {
     console.log("Conexão com o MongoDB Atlas estabelecida com sucesso.");
+
+    // Iniciar o servidor
     app.listen(port, () => {
         console.log(`SERVIDOR RODANDO NA PORTA ${port}`);
     });
-}).catch((error) => {
+})
+.catch((error) => {
     console.error("Erro na conexão com o MongoDB:", error);
 });
